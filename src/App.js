95321fcd217962affd4a6b57ec19d6cb7f3544f9
Matt Ashford <mattashford1@gmail.com>
2020-10-01 13:06:53 -0400

    starting point

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div id="appContainer">
+        <div id="sidebarContainer"> this the side div</div>
+        <div id="clockContainer"> this is the clock div</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
